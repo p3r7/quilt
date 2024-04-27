cd60ccbe81eccd3caf8c3867a3b8a416023b7146
perf <11557146+p3r7@users.noreply.github.com>
2024-04-27 13:27:56 +0200

    oupsie

--- lib/core.lua
+++ lib/core.lua
@@ -1,5 +1,24 @@
 
+-- ------------------------------------------------------------------------
+-- math
+
+function cos(x)
+  return math.cos(math.rad(x * 360))
+end
+
+function sin(x)
+  return -math.sin(math.rad(x * 360))
+end
+
 -- base1 modulo
 function mod1(v, m)
   return ((v - 1) % m) + 1
+end
+
+function linlin(slo, shi, dlo, dhi, f)
+  if slo < shi then
+    return util.linlin(slo, shi, dlo, dhi, f)
+  else
+    return - util.linlin(shi, slo, dlo, dhi, f)
+  end
 end
